Integrated visual.py into train.py to reduce code chunks. Updated train.py to have an argument paser, this made training more accessible and easier to test. Fixed loss calculation and evaluation functions.

recognition/siamese_s4810380/train.py
@@ -2,6 +2,9 @@
 # Training, validating, testing and saving
 #########################################
 
+import os
+import visual
+import argparse
 import torch
 import torch.nn as nn
 from torch.optim.adam import Adam
@@ -14,22 +17,36 @@ from torchvision import transforms
 import matplotlib.pyplot as plt
 from sklearn.metrics import accuracy_score, f1_score, roc_auc_score
 
-def contrastive_loss(y, d, margin=1.0):
-    """ The loss function utilised """
-    return torch.mean(y * d.pow(2) + (1 - y) * torch.clamp(margin - d, min=0).pow(2))
+def contrastive_loss(y, d, margin=1):
+    """ Gives the contrastive loss of a list of distances given a margin
+        and the ground truths.
 
-def train_siamese(train_load, val_load, model, model_name, device, n_epochs=5, show_plot=True):
+        @params:
+            y: Ground truth labels.
+            d: A tensor of the pairswise distances between the vectors
+                being compared.
+            margin: Threshold for how far dissimilar pairs should be.
+
+        @return:
+            The mean contrastive loss across the given pairs.
+    """
+    # https://www.geeksforgeeks.org/deep-learning/loss-functions-in-deep-learning/
+    # AI aided conversion to pytorch over numpy.
+    return 0.5 * torch.mean(y * d.pow(2) + (1 - y) * torch.clamp(margin - d, min=0).pow(2))
+
+def train_siamese(train_loader, val_loader, model_fp, device, n_epochs, verbose=True):
     """ The main training loop used to obtain a model.
 
         @params:
-            train_load: training set dataloader
-            val_load: validation set dataloader
-            model: the model to be trained
-            model_name: this will be used in the fp for the saved model
+            train_loader: training set dataloader
+            val_loader: validation set dataloader
+            model_fp: file path to save model
             device: what device is being used
-            show_plot: boolean value if the plot of loss across epochs
-                should be displayed
+            n_epochs: number of epochs to be trained for
+            verbose: boolean value if the train batch loss sohuld be
+                outputted to terminal
     """
+    model = SiameseNetwork().to(device)
     optimiser = Adam(model.parameters(), lr=1e-4)
 
     best_val_loss = float('inf')
@@ -40,16 +57,16 @@ def train_siamese(train_load, val_load, model, model_name, device, n_epochs=5, s
         model.train()
         total_train_loss = 0.0
 
-        for i, (img1, img2, label) in enumerate(train_load):
-            if not i % 50 and i != 0:
-                print(f"Train batch [{i}/{len(train_load)}], avg loss: {total_train_loss/i:.4f}")
+        for i, (img1, img2, label) in enumerate(train_loader):
+            if not i % 300 and i != 0 and verbose:
+                print(f"Train batch [{i}/{len(train_loader)}], avg loss: {total_train_loss/i:.4f}")
             img1, img2, label = img1.to(device), img2.to(device), label.to(device)
 
             # forward pass
             emb1, emb2 = model.forward(img1, img2)
-            distance = F.pairwise_distance(emb1, emb2)
 
             # compute loss
+            distance = F.pairwise_distance(emb1, emb2)
             loss = contrastive_loss(label, distance)
 
             # backpropagation
@@ -59,66 +76,34 @@ def train_siamese(train_load, val_load, model, model_name, device, n_epochs=5, s
 
             total_train_loss += loss.item()
 
-        avg_train_loss = total_train_loss / len(train_load)
-        print(f"Epoch [{epoch+1}/{n_epochs}] - Training loss: {avg_train_loss:.4f}")
+        avg_train_loss = total_train_loss / len(train_loader)
 
         model.eval()
         total_val_loss = 0.0
         with torch.no_grad():
-            for img1, img2, label in val_load:
+            for img1, img2, label in val_loader:
                 img1, img2, label = img1.to(device), img2.to(device), label.to(device)
                 emb1, emb2 = model(img1, img2)
                 distance = F.pairwise_distance(emb1, emb2)
                 loss = contrastive_loss(label, distance)
                 total_val_loss += loss.item()
 
-        avg_val_loss = total_val_loss / len(val_load)
-        print(f"Epoch [{epoch+1}/{n_epochs}] - Validation loss: {avg_val_loss:.4f}")
+        avg_val_loss = total_val_loss / len(val_loader)
+        print(f"Epoch [{epoch+1}/{n_epochs}] - Train loss: {avg_train_loss:.4f} | Val loss: {avg_val_loss:.4f}")
 
         if avg_val_loss < best_val_loss:
             best_val_loss = avg_val_loss
-            torch.save(model.state_dict(), f"{model_name}.pt")
+            torch.save(model.state_dict(), f"{model_fp}/siamese.pt")
             print("Saved new best model")
 
         train_losses.append(avg_train_loss)
         val_losses.append(avg_val_loss)
 
-    if show_plot:
-        plt.plot(range(1, n_epochs+1), train_losses, label='Train Loss')
-        plt.plot(range(1, n_epochs+1), val_losses, label='Validation Loss')
-        plt.xlabel('Epoch')
-        plt.ylabel('Contrastive Loss')
-        plt.title('Training & Validation Loss')
-        plt.legend()
-        plt.show()
+    visual.loss_plot(train_losses, val_losses, n_epochs, f"{model_fp}/siamese_loss")
 
-def eval_siamese(test_load, model, model_name, device):
-    """ Evaluates a model on given test data and outputs performance. 
-
-        @params:
-            test_load: the testing dataloader
-            model: model to be evaluated
-            model_name: the name of the model to be loaded
-            device: device being used
-    """
-    model.load_state_dict(torch.load(f"{model_name}.pt"))
-    model.eval()
-
-    test_loss = 0.0
-    with torch.no_grad():
-        for img1, img2, label in test_load:
-            img1, img2, label = img1.to(device), img2.to(device), label.to(device)
-            emb1, emb2 = model(img1, img2)
-            distance = F.pairwise_distance(emb1, emb2)
-            loss = contrastive_loss(label, distance)
-            test_loss += loss.item()
-
-    avg_test_loss = test_loss / len(test_load)
-    print(f"Test loss: {avg_test_loss:.4f}")
-
-def train_classifier(train_loader, val_loader, classifier_name, siamese_model_name, device, n_epochs=5):
+def train_classifier(train_loader, val_loader, model_fp, device, n_epochs, verbose):
     siamese = SiameseNetwork().to(device)
-    siamese.load_state_dict(torch.load(f"{siamese_model_name}.pt", map_location=device))
+    siamese.load_state_dict(torch.load(f"{model_fp}/siamese.pt", map_location=device))
 
     for param in siamese.base.parameters():
           param.requires_grad = False
@@ -129,15 +114,24 @@ def train_classifier(train_loader, val_loader, classifier_name, siamese_model_na
     criterion = nn.BCEWithLogitsLoss()
 
     best_val_loss = float('inf')
+    best_val_auc = 0.0
     train_losses = []
     val_losses = []
+    all_labels = []
+    all_preds = []
+    all_probs = []
+    val_aucs = []
+    val_f1s = []
 
     # Run main training loop
     for epoch in range(n_epochs):
         classifier.train()
         total_train_loss = 0
 
-        for imgs, labels in train_loader:
+        for i, (imgs, labels) in enumerate(train_loader):
+            if not i % 300 and i != 0 and verbose:
+                print(f"Train batch [{i}/{len(train_loader)}], avg loss: {total_train_loss/i:.4f}")
+
             # Alter label for BCE
             imgs, labels = imgs.to(device), labels.to(device).float().unsqueeze(1)
 
@@ -165,24 +159,60 @@ def train_classifier(train_loader, val_loader, classifier_name, siamese_model_na
                 loss = criterion(outputs, labels)
                 total_val_loss += loss.item()
 
+                probs = torch.sigmoid(outputs)
+                preds = (probs > 0.5).float()
+
+                all_labels.append(labels.cpu())
+                all_preds.append(preds.cpu())
+                all_probs.append(probs.cpu())
+
+        labels_np = torch.cat(all_labels).numpy()
+        preds_np = torch.cat(all_preds).numpy()
+        probs_np = torch.cat(all_probs).numpy()
+
+        val_f1 = f1_score(labels_np, preds_np)
+        val_auc = roc_auc_score(labels_np, probs_np)
+
+        val_f1s.append(val_f1)
+        val_aucs.append(val_auc)
+
+        if val_auc > best_val_auc:  
+            best_val_auc = val_auc
+            torch.save(classifier.state_dict(), f"{model_fp}/classifier.pt")
+            print("Saved new best model")
+
         avg_val_loss = total_val_loss / len(val_loader)
         val_losses.append(avg_val_loss)
 
         print(f"Epoch [{epoch+1}/{n_epochs}] - Train loss: {avg_train_loss:.4f} | Val loss: {avg_val_loss:.4f}")
+        print(f"Epoch [{epoch+1}/{n_epochs}] - F1: {val_f1:.4f} | AUC: {val_auc:.4f}")
 
-        if avg_val_loss < best_val_loss:
-            best_val_loss = avg_val_loss
-            torch.save(classifier.state_dict(), f"{classifier_name}.pt")
-            print("Saved new best classifier")
+    visual.loss_plot(train_losses, val_losses, n_epochs, f"{model_fp}/classifier_loss")
+    visual.score_plot(val_aucs, "Validation AUC", f"{model_fp}/val_auc")
+    visual.score_plot(val_f1s, "Validation F1", f"{model_fp}/val_f1")
 
-    if show_plot:
-        plt.plot(range(1, n_epochs+1), train_losses, label='Train Loss')
-        plt.plot(range(1, n_epochs+1), val_losses, label='Validation Loss')
-        plt.xlabel('Epoch')
-        plt.ylabel('Contrastive Loss')
-        plt.title('Training & Validation Loss')
-        plt.legend()
-        plt.show()
+def eval_siamese(test_loader, model, device):
+    """ Evaluates a model on given test data and outputs performance. 
+
+        @params:
+            test_loader: the testing dataloader
+            model: model to be evaluated
+            device: device being used
+    """
+    model.eval()
+
+    test_loss = 0.0
+    with torch.no_grad():
+        for img1, img2, label in test_loader:
+            img1, img2, label = img1.to(device), img2.to(device), label.to(device)
+            emb1, emb2 = model(img1, img2)
+            distance = F.pairwise_distance(emb1, emb2)
+            loss = contrastive_loss(label, distance)
+            test_loss += loss.item()
+
+    avg_test_loss = test_loss / len(test_loader)
+    print("\nSiamese")
+    print(f"Test loss: {avg_test_loss:.4f}")
 
 def eval_classifier(test_loader, siamese, classifier, device):
     siamese.eval()
@@ -201,17 +231,71 @@ def eval_classifier(test_loader, siamese, classifier, device):
             y_true.extend(labels.numpy())
             y_pred.extend(preds)
 
+    print("\nClassifier")
     print(f"Accuracy: {accuracy_score(y_true, y_pred):.4f}")
     print(f"F1 Score: {f1_score(y_true, y_pred):.4f}")
     print(f"ROC-AUC: {roc_auc_score(y_true, y_pred):.4f}")
 
 if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(
+        description="Train and evaluate Siamese network and BinaryClassifer"
+    )
+
+    # Model name arguments
+    parser.add_argument(
+        "-m",
+        type=str,
+        default="0",
+        help="Model name of directory to be saved/loaded."
+    )
+
+    # Options
+    parser.add_argument(
+        "-e",
+        type=int,
+        default=5,
+        help="Number of epochs to train each model."
+    )
+    parser.add_argument(
+        "-v",
+        action="store_true",
+        help="If set, enables verbose output during training."
+    )
+    parser.add_argument(
+        "-train",
+        action="store_true",
+        help="If set, trains models."
+    )
+    parser.add_argument(
+        "-eval",
+        action="store_true",
+        help="If set, performs test evaluation on models."
+    )   
+    parser.add_argument(
+        "-p",
+        action="store_true",
+        help="If set, plots performance of models and saves."
+    )   
+
+    args = parser.parse_args()
+
+    # Assign args
+    model_name = args.m
+    n_epochs = args.e
+    verbose = args.v
+    training = args.train
+    evaluation = args.eval
+    plots = args.p
+
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     img_dir = "./data/train-image/image"
     metadata = "./data/train-metadata.csv"
-    siamese_model_name = "siamese_model"
-    classifier_model_name = "classifier_model"
+
+    # Make model directory to store models if not already there
+    model_fp = f"./model/{model_name}"
+    os.makedirs(model_fp, exist_ok=True)
 
     train_transform = transforms.Compose([
         transforms.Resize((224, 224)), # Make sure images are 224x224
@@ -238,30 +322,28 @@ if __name__ == "__main__":
         train_transform,
         val_transform,
         batch_size=16,
-        num_workers=1,
-        train_split=0.7,
-        val_split=0.1,
-        test_split=0.2,
+        num_workers=4,
         seed=7
     )
 
-    n_epochs = 5
-    model = SiameseNetwork().to(device)
-    train_siamese(siamese_train, siamese_val, model, siamese_model_name, device, n_epochs)
-    #eval_siamese(test_load, model, model_name, device)
-
-    n_epochs = 5
-    train_classifier(classifier_train, classifier_val, classifier_model_name,
-                     siamese_model_name, device, n_epochs)
+    if training:
+        train_siamese(siamese_train, siamese_val, model_fp, device, n_epochs, verbose)
+        train_classifier(classifier_train, classifier_val, model_fp, device, n_epochs, verbose)
 
     siamese = SiameseNetwork().to(device)
-    siamese.load_state_dict(torch.load(f"{siamese_model_name}.pt", map_location=device))
+    siamese.load_state_dict(torch.load(f"{model_fp}/siamese.pt", map_location=device))
     classifier = BinaryClassifier().to(device)
-    classifier.load_state_dict(torch.load(f"{classifier_model_name}.pt", map_location=device))
+    classifier.load_state_dict(torch.load(f"{model_fp}/classifier.pt", map_location=device))
 
     # Freeze Parameters of base
     # https://discuss.pytorch.org/t/how-the-pytorch-freeze-network-in-some-layers-only-the-rest-of-the-training/7088
     for param in siamese.base.parameters():
         param.requires_grad = False
 
-    eval_classifier(classifier_test, siamese, classifier, device)
+    if evaluation: 
+        eval_siamese(siamese_test, siamese, device)
+        eval_classifier(classifier_test, siamese, classifier, device)
+
+    if plots:
+        visual.tsne_embeddings(classifier_test, siamese, device, f"{model_fp}/tsne")
+        visual.classifier_confusion(classifier_test, classifier, siamese, device, f"{model_fp}/cm")